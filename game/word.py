bc9f2f513e3bd73d02e80a92f9d60b4e3d3adc6c
Grant Shirley <grantmshirley516@gmail.com>
2021-05-20 12:30:57 -0600

    started working on the skeleton of the word class

--- game/word.py
+++ game/word.py
@@ -1,1 +1,17 @@
 import random
+
+class Word:
+
+    def __init__(self):
+        word = "hello"
+        guess_word = [0]
+        wrong_guesses = len(word)
+
+    def update_word(new_word):
+        pass
+
+    def guessed_letter(letter):
+        pass
+
+    def keep_playing():
+        pass
