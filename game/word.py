fd5ef17348eaafb0cfdf3a23baa9d0679e518e73
Grant Shirley <grantmshirley516@gmail.com>
2021-05-20 21:11:02 -0600

    The beta type for the word class

--- game/word.py
+++ game/word.py
@@ -6,7 +6,7 @@ class Word:
         word = "hello"
         guess_word = []
         show_guess = ""
-        wrong_guesses = len(word)
+        wrong_guesses = 0
 
     def update_word(self):
         """This class chooses a word for the game and then makes the the tiles."""
@@ -18,14 +18,33 @@ class Word:
         elif val == 3:
             self.word = "pikachu"
 
+        self.wrong_guesses = len(self.word)
+
         #use the code below to also update when the player chooses a correct guess
         for x in range(0, len(self.word)):
             self.guessed_word.append("_ ")
             self.show_guess = self.show_guess + self.guessed_word[x]
         
 
-    def guessed_letter(letter):
-        pass
+    def guessed_letter(self, letter):
 
-    def keep_playing():
-        pass
+        wrong =0
+        
+        for x in range(0, len(self.word)):
+            if letter == self.word[x]:
+                self.guessed_word[x] = letter
+                self.show_guess = ""
+                self.show_guess = self.show_guess + self.guessed_word[x]
+            else:
+                wrong += 1
+
+            if(wrong == len(self.word)):
+                self.wrong_guesses = self.wrong_guesses -1
+
+
+    def keep_playing(self):
+        
+        if(self.wrong_guesses == 0):
+            return(False)
+        else:
+            return(True)
